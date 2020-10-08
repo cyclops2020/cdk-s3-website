263ed7a6a940bf2cae4477388f11d08c41c50bbf
Scott Tate <scott.tate@blndspt.com>
2020-10-07 21:04:34 -0600

    fixed files

--- src/cdk-s3-website.ts
+++ src/cdk-s3-website.ts
@@ -1,2 +1,22 @@
-import { S3WebsiteBucket } from '.';
-export { S3WebsiteBucket };
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+export interface S3WebsiteBucketProps extends cdk.StackProps {
+  bucketName?: string;
+  WebsiteIndexDocument: string,
+  WebsiteErrorDocument: string
+}
+
+export class S3WebsiteBucket extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: S3WebsiteBucketProps) {
+    super(scope, id);
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `${props.bucketName}-${Math.floor(Math.random() * Math.floor(1000000))}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: props.WebsiteIndexDocument,
+      websiteErrorDocument: props.WebsiteErrorDocument
+  });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import { S3WebsiteBucket } from './cdk-s3-website';
+export { S3WebsiteBucket };
 
-export interface S3WebsiteBucketProps extends cdk.StackProps {
-  bucketName?: string;
-  WebsiteIndexDocument: string,
-  WebsiteErrorDocument: string
-}
 
-export class S3WebsiteBucket extends cdk.Construct {
-
-  constructor(scope: cdk.Construct, id: string, props: S3WebsiteBucketProps) {
-    super(scope, id);
-
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-      bucketName: `${props.bucketName}-${Math.floor(Math.random() * Math.floor(1000000))}`,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: props.WebsiteIndexDocument,
-      websiteErrorDocument: props.WebsiteErrorDocument
-  });
-  }
-}
