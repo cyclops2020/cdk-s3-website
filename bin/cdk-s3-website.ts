aaf39724d0339248434bd5596125f8db7790f399
Scott Tate <scott.tate@blndspt.com>
2020-10-07 20:35:12 -0600

    first commit

--- bin/cdk-s3-website.ts
+++ bin/cdk-s3-website.ts
@@ -1,7 +1,2 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkS3WebsiteStack } from '../lib/cdk-s3-website-stack';
-
-const app = new cdk.App();
-new CdkS3WebsiteStack(app, 'CdkS3WebsiteStack');
+import { S3WebsiteBucket } from '../lib/cdk-s3-website-stack';
+export { S3WebsiteBucket };
--- lib/cdk-s3-website-stack.ts
+++ lib/cdk-s3-website-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
-export class CdkS3WebsiteStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export interface S3WebsiteBucketProps extends cdk.StackProps {
+  bucketName?: string;
+  WebsiteIndexDocument: string,
+  WebsiteErrorDocument: string
+}
 
-    // The code that defines your stack goes here
+export class S3WebsiteBucket extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: S3WebsiteBucketProps) {
+    super(scope, id);
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `${props.bucketName}-${Math.floor(Math.random() * Math.floor(1000000))}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: props.WebsiteIndexDocument,
+      websiteErrorDocument: props.WebsiteErrorDocument
+  });
   }
 }
